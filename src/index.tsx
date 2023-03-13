4ee05a8979c8080c3bd077690a27bd640899eaaf
Batonec <pasapaval@gmail.com>
2023-03-14 02:07:48 +0300

    intial commit

---
+++ src/index.tsx
@@ -0,0 +1,98 @@
+import React, {memo} from 'react';
+import {Typography, Card, Space} from 'antd';
+import {
+  Layout,
+  PluginClient,
+  usePlugin,
+  createState,
+  useValue,
+  theme,
+  styled,
+  DataInspector,
+  DetailSidebar
+} from 'flipper-plugin';
+
+type NavState = {
+  currentStackId?: string;
+  stacks: Stack[];
+  dialog?: string;
+};
+
+type Stack = {
+  id: string;
+  chain: Screen[];
+}
+
+type Screen = {
+  id: string;
+}
+
+type Events = {
+  alcubierreNavState: NavState;
+};
+
+export function plugin(client: PluginClient<Events, {}>) {
+  const data = createState<NavState>({ stacks: [] });
+  client.onMessage('alcubierreNavState', (state) => { data.set(state); console.log(state); });
+  return {data};
+}
+
+export function Component() {
+  const instance = usePlugin(plugin);
+  const data = useValue(instance.data);
+
+  return (
+    <>
+      <Layout.ScrollContainer
+        vertical
+        style={{background: theme.backgroundWash}}>
+        <Layout.Horizontal gap pad style={{flexWrap: 'wrap'}}>
+          {data.stacks.map((stack) => (
+
+            <Layout.Container padh >
+
+            <Card
+              hoverable
+              data-testid={stack.id}
+              title={'Stack ID: ' + stack.id}
+              style={{
+                minWidth: 230,
+                height: 50,
+                borderColor: stack.id == data.currentStackId ? "0x00FFAA" : theme.primaryColor,
+                textAlign: 'center',
+              }}>
+            </Card>
+
+            <Space
+              style={{
+                background: undefined,
+                height: 10,
+                borderColor: undefined
+              }}>
+            </Space>
+            
+            {  
+              stack.chain.map((screen) => (
+
+                  <Card
+                    hoverable
+                    data-testid={screen.id}
+                    title={''+screen.id}
+                    style={{
+                      width: 230,
+                      height: 50,
+
+                      borderColor: theme.primaryColor,
+                      textAlign: 'center',
+                    }}>
+                  </Card>
+
+              ))
+            }
+            </Layout.Container>
+          ))}
+        </Layout.Horizontal>
+      </Layout.ScrollContainer>
+    </>
+  );
+}
